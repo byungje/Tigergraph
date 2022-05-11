4bcd94967effbbeb2ce2165f39f8808638e5e758
byungje <79308906+byungje@users.noreply.github.com>
2022-05-11 13:31:39 +0900

    Add files via upload

---
+++ StarterKit_Demo/Customer360/03_load_data.sh
@@ -0,0 +1,35 @@
+time = `date`
+echo $time
+
+## PLEASE modify the line below to the directory where your raw file sits and remove the '#'
+export customer360_data_dir=/home/tigergraph/04_Customer360/Custome360_data/
+
+
+#start all TigerGraph services
+gadmin start all
+
+gsql -g customer "run loading job load_job_Lead using
+v_Lead=\"${customer360_data_dir}/Lead.csv\""
+
+gsql -g customer "run loading job load_job_OpportunityContactRole using
+v_OpportunityContactRole=\"${customer360_data_dir}/OpportunityContactRole.csv\""
+
+gsql -g customer "run loading job load_job_Campaign using
+v_Campaign=\"${customer360_data_dir}/Campaign.csv\""
+
+gsql -g customer "run loading job load_job_CampaignMember using
+v_CampaignMember=\"${customer360_data_dir}/CampaignMember.csv\""
+
+gsql -g customer "run loading job load_job_Opportunity using
+v_Opportunity=\"${customer360_data_dir}/Opportunity.csv\""
+
+gsql -g customer "run loading job load_job_Account using
+v_Account=\"${customer360_data_dir}/Account.csv\""
+
+gsql -g customer "run loading job load_job_Contact using
+v_Contact=\"${customer360_data_dir}/Contact.csv\""
+
+
+time = `date`
+echo $time
+
