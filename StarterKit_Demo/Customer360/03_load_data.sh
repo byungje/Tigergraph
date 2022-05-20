8567be2c139f1a18a1fa4c95b0e7e6fd5e2c62cb
byungje <79308906+byungje@users.noreply.github.com>
2022-05-20 16:06:15 +0900

    Update 03_load_data.sh

--- StarterKit_Demo/Customer360/03_load_data.sh
+++ StarterKit_Demo/Customer360/03_load_data.sh
@@ -1,5 +1,4 @@
-time = `date`
-echo $time
+echo "data now : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)"
 
 ## PLEASE modify the line below to the directory where your raw file sits and remove the '#'
 export customer360_data_dir=/home/tigergraph/04_Customer360/Custome360_data/
@@ -29,7 +28,5 @@ v_Account=\"${customer360_data_dir}/Account.csv\""
 gsql -g customer "run loading job load_job_Contact using
 v_Contact=\"${customer360_data_dir}/Contact.csv\""
 
-
-time = `date`
-echo $time
+echo "data now : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)"
 
